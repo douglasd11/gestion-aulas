324b543c9e80ee67dbb7293ab6d858ddc1c5bd6d
WenBE <wendybuelvase@gmail.com>
2024-11-14 15:37:48 -0500

    admin

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import HorarioB from "./pages/HorarioB";
 import HorarioS from "./pages/HorarioS";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ReservaA from "./pages/ReservaA";
 import ReservaU from "./pages/ReservaU";
 
 const App = () => {
   return (
     <>
-      <HorarioB></HorarioB>
+      {/* <HorarioB></HorarioB>
       <HorarioS></HorarioS>
       <Login></Login>
       <Register></Register>
-      <ReservaU></ReservaU>
+      <ReservaU></ReservaU> */}
+      <ReservaA></ReservaA>
     </>
   );
 };
